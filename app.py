eb65ecaa2d6983ad4836a0702effd9bd57c4bba6
winonecheng <F74036328@mail.ncku.edu.tw>
2018-12-17 19:14:14 +0800

    Modify image file type from png to svg

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ def webhook_handler():
 
 @route('/show-fsm', methods=['GET'])
 def show_fsm():
-    machine.get_graph().draw('fsm.png', prog='dot', format='png')
-    return static_file('fsm.svg', root='./', mimetype='image/png')
+    machine.get_graph().draw('fsm.svg', prog='dot', format='svg')
+    return static_file('fsm.svg', root='./', mimetype='image/svg')
 
 
 if __name__ == "__main__":
